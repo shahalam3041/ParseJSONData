cb499ab2d0406a3a3dee2661fd1e63874abae04e
shahalam3041@gmail.com <shahalam3041@gmail.com>
2018-06-01 02:10:53 +0600

    successfully parse json data without formatting

--- app/src/main/java/com/example/shahalam/parsejsondata/MainActivity.java
+++ app/src/main/java/com/example/shahalam/parsejsondata/MainActivity.java
@@ -22,13 +22,54 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         t1 = findViewById(R.id.t1);
+
+        jsonTask jTask = new jsonTask();
+        jTask.execute();
     }
 
     public class jsonTask extends AsyncTask<String,String,String> {
 
         @Override
         protected String doInBackground(String... strings) {
+
+            HttpURLConnection httpURLConnection = null;
+            BufferedReader bufferedReader = null;
+
+            try {
+                URL url = new URL("https://api.myjson.com/bins/xcvpq");
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuffer stringBuffer = new StringBuffer();
+                String line = "";
+                while ((line = bufferedReader.readLine() )!= null ) {
+
+                    stringBuffer.append(line);
+                }
+
+                return stringBuffer.toString();
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+                httpURLConnection.disconnect();
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
             return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            t1.setText(s);
         }
     }
 }
